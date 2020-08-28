3c819b60950f6e226dcef46c9e1af3dac314a589
Alan Savage <65185890+alan-pie@users.noreply.github.com>
2020-08-28 10:13:52 -0700

    more examples

--- spec/models/bar_spec.rb
+++ spec/models/bar_spec.rb
@@ -1,33 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Bar, type: :model do
-  describe 'a new Bar with a foo in foos' do
-      let(:foo) { Foo.new }
-      let(:bar) { Bar.new(foos: [foo]) }
+  describe 'with a foo in foos,' do
+    let(:foo) { Foo.new }
+    let(:bar) { Bar.new(foos: [foo]) }
 
-      it 'foo_ids.include?(foo.id) includes the foo' do
-        expect(bar.foo_ids.include?(foo.id)).to be true
-      end
+    it 'foo_ids.include?(foo.id) includes the foo' do
+      expect(bar.foo_ids.include?(foo.id)).to be true
+    end
 
-      it 'foos.first is foo' do
-        expect(bar.foos.first).to eq foo
-      end
+    it 'foos.first is foo' do
+      expect(bar.foos.first).to eq foo
+    end
 
-      it 'foos.empty? is false' do
-        expect(bar.foos.empty?).to be false
-      end
+    it 'foos.size is 1' do
+      expect(bar.foos.size).to eq 1
+    end
 
-      it 'foos.find(foo.id) returns the foo' do
-        expect(bar.foos.find(foo.id)).to eq foo
-      end
+    it 'foos.empty? is false' do
+      expect(bar.foos.empty?).to be false
+    end
 
-      it 'foos.include?(foo) is true' do
-        expect(bar.foos.include?(foo)).to be true
-      end
+    it 'foos.find(foo.id) returns the foo' do
+      expect(bar.foos.find(foo.id)).to eq foo
+    end
 
-      it 'foos.exists?(foo.id) is true' do
-        expect(bar.foos.exists?(foo.id)).to be true
-      end
+    it 'foos.include?(foo) is true' do
+      expect(bar.foos.include?(foo)).to be true
+    end
+
+    it 'foos.exists?(foo.id) is true' do
+      expect(bar.foos.exists?(foo.id)).to be true
+    end
+
+    it 'foos.take returns f' do
+      expect(bar.foos.take).to eq f
     end
   end
 end
